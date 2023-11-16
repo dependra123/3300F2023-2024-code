02e9f9caa4d07fc568f4a76f6a60561aad874b43
Arin Johar <johararin@gmail.com>
2023-11-15 18:38:39 -0700

    afa

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
- *
+ *lib/api.hpp"
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
